1d8faa5fbd14d3aa73a668e73a415d4a3c161c25
Circl3s <maak4422@gmail.com>
2022-09-24 03:21:41 +0200

    Windows internal changes

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,9 +25,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
+  Win32Window::Point origin(30, 30);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"neko_launcher_neo", origin, size)) {
+  if (!window.CreateAndShow(L"Neko Launcher Neo", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
